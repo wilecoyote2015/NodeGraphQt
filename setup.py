e19d8bda4c282f2610b1ca00805412c582cd85d4
bjoern <wilecoyote2015@gmail.com>
2022-07-27 11:48:49 +0200

    fix install

--- setup.py
+++ setup.py
@@ -2,7 +2,16 @@
 # -*- coding: utf-8 -*-
 import setuptools
 
-from NodeGraphQt import pkg_info
+class pkg_info:
+    __version__ = '0.2.5'
+    __status__ = 'Work in Progress'
+    __license__ = 'MIT'
+
+    __author__ = 'Johnny Chan'
+
+    __module_name__ = 'NodeGraphQt'
+    __url__ = 'https://github.com/jchanvfx/NodeGraphQt'
+
 
 if __name__ == '__main__':
     setuptools.setup(
